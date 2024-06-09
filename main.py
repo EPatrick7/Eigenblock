162223f2da8a12f230a5126e001ca803032ae531
Eric Patrick <ericmpatrick777@gmail.com>
2024-06-09 02:57:01 -0700

    Expanded Database Support
    Extended support to databases of human face images, not just Minecraft blocks.

--- main.py
+++ main.py
@@ -4,20 +4,42 @@ import tkinter as tk
 from tkinter import Entry,Button, Frame, Scrollbar, messagebox,Scale
 import matplotlib.pyplot as plt
 import os
+from math import sqrt
 import glob
 plt.rcParams['toolbar'] = 'None'
 
+max_images=5000
 
-n=16
-texture_folder="textures"
+n=250
+texture_folder="lfw-deepfunneled"
+current_format="jpg"
+base_block="Aaron_Eckhart\Aaron_Eckhart_0001"
+base_block2="George_W_Bush\George_W_Bush_0001"
 
-base_block="dirt"
-base_block2="diamond_block"
+
+#n=100
+#texture_folder="faces"
+#current_format="jpg"
+#base_block="001_03"
+#base_block2="002_03"
+
+
+#n=16
+#texture_folder="textures"
+#current_format="png"
+
+#base_block="dirt"
+#base_block2="diamond_block"
+
+global brightness_offset
+brightness_offset=0
 
 
 
 def output_plot(matrix):
     '''Renders a matrix as a matplot plot'''
+    matrix+=np.ones_like(matrix)*brightness_offset
+    
     plt.figure("EigenBlock Construction")
     plt.imshow(matrix, cmap='gray', vmin=0, vmax=255)
     plt.axis('off')
@@ -44,21 +66,30 @@ def read_texture(file_path):
           
             
 def read_textures():
-    '''Inputs all png files of nxn into a matrix in mean-deviation form'''
-    textures = glob.glob(os.path.join(texture_folder+"/", '*.png'))
+    '''Inputs all files of nxn into a matrix in mean-deviation form'''
+    textures = glob.glob(os.path.join(texture_folder+"/", '*.'+current_format), recursive=True)
     all_textures=[]
-
-    for file_path in textures:
-        image = Image.open(file_path)
-        if image.size == (n, n): #Throw out all not nxn images.
-            image_gray = image.convert('RGBA').convert('L')
-            matrix = np.array(image_gray)
-            
-            
-            all_textures.append(matrix.flatten())
-        
+    #print("Loading Images...")
+    i=0
+    for root, _, files in os.walk(texture_folder):
+        if i >=max_images:
+            break
+        for file_name in files:
+            if i >=max_images:
+                break
+            file_path = os.path.join(root, file_name)
+            print(file_path)
+            if file_name.endswith('.' + current_format):
+                image = Image.open(file_path)
+                if image.size == (n, n): # Throw out all images not of size nxn.
+                    image_gray = image.convert('RGBA').convert('L')
+                    matrix = np.array(image_gray)
+                    i+=1
+                    all_textures.append(matrix.flatten())
+    if len(all_textures)<=0:
+        raise ValueError("There were no files found that fit " +texture_folder +"/*."+current_format)
     all_textures=np.vstack(all_textures)
-
+    print("Performing SVD...")
     row_means = np.mean(all_textures, axis=1, keepdims=True)
     
     return all_textures - row_means,row_means
@@ -89,7 +120,7 @@ def read_block():
     '''Updates w to the current base_block id'''
     global w,w_last
     w_last=None
-    w= Vh@(read_texture("textures/"+base_block+".png"))
+    w= Vh@(read_texture(texture_folder+"/"+base_block+"."+current_format))
     return w
 
 read_block()
@@ -126,11 +157,23 @@ def on_configure(event):
     canvas.configure(scrollregion=canvas.bbox('all'))
 inner_frame.bind('<Configure>', on_configure)
 
-num_rows = n
-num_cols = n
+num_rows = round(sqrt(w.size))
+num_cols = w.size/num_rows
+if num_cols != round(num_cols):
+    num_cols=int(num_cols)+1
+num_cols=int(num_cols)
+
+print("Setting Up Tkinter: "+str(num_rows)+"x"+str(num_cols))
+
+
 w_entries = []
 
 sv=round_out(Svals)
+
+
+def update_brightness(event):
+    global brightness_offset
+    brightness_offset=float(brightness_block.get())
 
 def update_plot(event):
     plot_reconstructed_texture()
@@ -139,6 +182,8 @@ def update_plot(event):
 for i in range(num_rows):
     row_entries = []
     for j in range(num_cols):
+        if i*num_cols+j>=w.size:
+            break
         entry = Entry(inner_frame, width=10)
         val=round(w[i * num_cols + j],2)
         
@@ -149,7 +194,10 @@ for i in range(num_rows):
         entry.grid(row=i, column=j, padx=5, pady=5)
         entry.bind('<KeyRelease>', update_plot)
         row_entries.append(entry)
+    if i*num_cols+j>=w.size:
+        break
     w_entries.append(row_entries)
+    
 
 
 
@@ -164,6 +212,8 @@ def plot_reconstructed_texture():
                 entry.insert(0,str(0))
                 val=0
             w_values.append(float(val))
+    while len(w_values) < w.size:
+        w_values.append(0)
             
     if (not w_last is None) and np.linalg.norm(np.array(w_values)-w_last)==0:
         return
@@ -193,14 +243,14 @@ def update_selected_block():
     try:
         read_block()
     except:
-        messagebox.showerror("Block Not Found", base_block+".png was not found!")
+        messagebox.showerror("Block Not Found", base_block+"."+current_format+" was not found!")
         return
 
     i=0
     for row in w_entries:
         j=0
         for entry in row:
-            val=round(w[i * num_cols + j],2)
+            val=round(w[i * num_cols + j],5)
 
             entry.delete(0,tk.END)
             entry.insert(0,str(val))
@@ -228,7 +278,7 @@ def blend_selected_block():
             w=read_block()
         
     except:
-        messagebox.showerror("Block Not Found", base_block+".png was not found!")
+        messagebox.showerror("Block Not Found", base_block+"."+current_format+" was not found!")
         return
     
     p=min(max(0,float(blend_slider.get())/100.0),1)
@@ -279,6 +329,13 @@ blend_slider.pack(side=tk.LEFT, padx=5)
 
 load_preset_button2 = Button(root, text="Blend Presets", command=blend_selected_block)
 load_preset_button2.pack(side=tk.LEFT, padx=5)
+
+
+brightness_block = Entry(root, width=20)
+brightness_block.insert(0, str(0))
+brightness_block.bind('<KeyRelease>', update_brightness)
+brightness_block.pack(side=tk.LEFT, padx=5)
+
 
 
 
